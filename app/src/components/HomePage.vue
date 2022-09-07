dc1bfff0ae543841833b78f03b3485e20e29db21
Leon Glaser <leon.glaser@gmail.com>
2022-09-07 15:02:07 -0400

    axios call added to homepage

--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -1,18 +1,32 @@
 <template>
     <h1>Here's the home page</h1>
     <div>
-        <ScrollCard>Test ScrollCard</ScrollCard>
+        <ScrollCard msg="HomePage scroll test"></ScrollCard>
     </div>
 </template>
 
 <script>
-// import axios from 'axios'
-// import { BASE_URL } from '../globals'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 import ScrollCard from '../components/ScrollCard.vue'
 
 export default {
     name: "HomePage",
     components: { ScrollCard },
+    data() {
+        return {
+            scrolls: [],
+        };
+    },
+
+    async created() {
+        try {
+            const res = await axios.get(`${BASE_URL}/scrolls`)
+            this.scrolls = res.data
+        } catch (error) {
+            console.log(error)
+        }
+    },
     methods: {
 
     }
