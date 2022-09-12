124c272c9d4ad904511e5016ed7c4a0d1c9dab2f
Leon Glaser <leon.glaser@gmail.com>
2022-09-12 12:06:51 -0400

    new branch, adding chapter functionality

--- src/components/ScrollCard.vue
+++ src/components/ScrollCard.vue
@@ -12,7 +12,7 @@
             <button @click="$router.push(`/scrolls/users/7/scrolls/${result.id}`)" :body="result.body"
                 :title="result.title" :id="result.id">Edit</button>
             <!-- <router-link to="/scrolls/users/7/scrolls/:scroll_id">Edit</router-link> -->
-            <button>Add Chapter</button>
+            <button>View chapters</button>
             <button @click="$router.push(`/scrolls/users/7/scrolls/delete/${result.id}`)" :body="result.body"
                 :title="result.title" :id="result.id">Delete</button>
         </div>
@@ -45,4 +45,5 @@ export default {
 </script>
 
 <style>
+
 </style>
--- src/components/ScrollPage.vue
+++ src/components/ScrollPage.vue
@@ -1,14 +1,24 @@
 <!-- This page will render a scroll and all its chapters. -->
 <template>
     <div>
-        <!-- Loop through various scroll cards -->
+        <!-- Show selected scroll content, and loop through chapter cards -->
+        <ScrollCard></ScrollCard>
+        <ChapterCard></ChapterCard>
     </div>
 </template>
 
 <script>
+import ScrollCard from "./ScrollCard.vue";
+import ChapterCard from "./ChapterCard.vue";
 
+
+export default {
+    name: "ScrollPage",
+    components: { ScrollCard, ChapterCard }
+}
 
 </script>
 
 <style>
+
 </style>
