cedd1b0092531cb1048be7a66f429e1e2e4dce06
Leon Glaser <leon.glaser@gmail.com>
2022-09-14 10:08:38 -0400

    clean up delete scroll page

--- src/components/DeleteScrollPage.vue
+++ src/components/DeleteScrollPage.vue
@@ -37,21 +37,10 @@ export default {
             console.log(error);
         }
     },
-    // async created() {
-    //     try {
-    //         const res = await axios.get(`${BASE_URL}/scrolls/users/7/scrolls`)
-    //         this.scrolls = res.data
-    //         console.log(res.data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // },
+
     methods: {
         // 
         async deleteScroll() {
-            console.log(this.scrollTitle);
-            console.log(this.scrollBody);
-            console.log(this.$route.params.scroll_id)
 
             const res = await axios.delete(`${BASE_URL}/scrolls/users/7/scrolls/${this.$route.params.scroll_id}`, { title: this.scrollTitle, body: this.scrollBody })
             console.log(res)
